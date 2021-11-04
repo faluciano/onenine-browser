75624cad5643822b4f5a0fdcb0b2acaaa10bfd02
Felix Luciano <felixluciano.a@gmail.com>
2021-11-04 00:27:34 -0400

    Trying to fetch browser.html now checks if a user data folder data has been crated and creates on if not

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,29 +8,27 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-
+import os
 
 @login_required(login_url="/login/")
 def index(request):
     context = {'segment': 'index'}
-    print(f"req {request}")
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
 
 
 @login_required(login_url="/login/")
 def pages(request):
-    context = {'test':'testing'}
+    context = {'user':request.user}
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
     try:
-        print(f"req {request}")
         load_template = request.path.split('/')[-1]
-
         if load_template == 'admin':
             return HttpResponseRedirect(reverse('admin:index'))
         context['segment'] = load_template
-
+        if load_template == 'browser.html':
+            return browser(request,context,load_template)
         html_template = loader.get_template('home/' + load_template)
         return HttpResponse(html_template.render(context, request))
 
@@ -42,3 +40,11 @@ def pages(request):
     except:
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
+
+def browser(request,context,load_template):
+    if not os.path.exists('onenine_priv'):
+        os.mkdir('onenine_priv')
+    if not os.path.exists(f'onenine_priv/{request.user}'):
+        os.mkdir(f'onenine_priv/{request.user}')
+    html_template = loader.get_template('home/' + load_template)
+    return HttpResponse(html_template.render(context, request))
